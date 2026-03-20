4b198ec525315b8a7dca257a6154114c99b811b1
thxxmlk30 <thxxmlk30@gmail.com>
2026-03-20 13:29:43 +0000

    Mise en place de react-router-dom et des routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAuth } from './context/AuthContext';
+import
+<ProtectedRoute>
+<DashboardPage /></ProtectedRoute>om './pages/LandingPage';
+import Connexion from './pages/auth/Connexion';
+import Inscription from './pages/auth/inscription';
+import MdpOublie from './pages/auth/MdpOublie';
+import DashboardPage from './pages/dashboard/DashboardPage';
 
-function App() {
+// Composant qui protège les routes privées
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <>{children}</> : <Navigate to="/login" replace />;
+}
+
+export default function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/connexion' element={<Connexion />} />
+      <Route path='/inscription' element={<Inscription />} />
+      <Route path='/mdp-oublie' element={<MdpOublie />} />
+      <Route path='/dashboard' element={
+        <ProtectedRoute>
+          <DashboardPage />
+        </ProtectedRoute>
+      } />
+    </Routes>
   )
 }
 
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useState, ReactNode } from 'react';
+import { User } from '../types';
+
+interface AuthContextType {
+  user: User | null;
+  login: (email: string, password: string) => boolean;
+  logout: () => void;
+  isAuthenticated: boolean;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children } : {children:ReactNode}){
+    const [user,setUser] = useState<User | null>(null);
+    const login = (email:string,password:string):boolean => {
+        if(email === 'admin@restaurant.com' && password === 'Resataurant1234'){
+            setUser({
+                id : '1',
+                nom : 'Chef Admin',
+                email : 'admin@restaurant.com',
+                role : 'admin'
+            })
+            return true;
+        }
+        return false;
+    };
+
+    const logout = () =>{
+        setUser(null);
+    };
+    return(
+        <AuthContext.Provider value={{user,login,logout,isAuthenticated:!!user}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export function useAuth(){
+    const context = useContext(AuthContext);
+    return context;
+}
